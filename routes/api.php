42b1dce695c83c8a2a4430b0a6461e8fb51beee2
FAHD <mahfoudifahd55@gmail.com>
2023-03-10 17:06:25 +0100

    reset password functionality done

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
-Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+// Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
+
+Route::post('/forgot',[AuthController::class,'forgot']);
+Route::post('/reset',[AuthController::class,'reset']);
+
+
+
+Route::group(['controller' => ResetPasswordController::class], function (){
+    // Request password reset link
+    Route::post('forgot-password', 'sendResetLinkEmail')->middleware('guest')->name('password.email');
+    // Reset password
+    Route::post('reset-password', 'resetPassword')->middleware('guest')->name('password.update');
+
+    Route::get('reset-password/{token}', function (string $token) {
+         return $token;
+     })->middleware('guest')->name('password.reset');
+});
