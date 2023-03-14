76f3386dd739cf9f8da669cf719a40f1cd0705f5
FAHD <mahfoudifahd55@gmail.com>
2023-03-14 17:28:30 +0100

    update fixed and add destroy method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -109,6 +109,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json([
+            'message' => 'Product deleted successfully',
+        ]);
     }
 }
